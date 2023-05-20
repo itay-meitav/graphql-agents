d770f8a9fa42e314b95629323a30f70bb021e0fa
Itay Meitav <itai9999@gmail.com>
2023-05-20 15:13:42 +0300

    Added Apollo Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
+import axios from "axios";
+import config from "./assets/config";
+import { gql, useQuery } from "@apollo/client";
 import "./styles.scss";
 
 function App() {
-  return <div class="grid-container"></div>;
+  const AGENT = gql`
+    query {
+      agent {
+        id
+        first_name
+        last_name
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(AGENT);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  if (data) console.log(data);
+
+  return <div className="grid-container"></div>;
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import config from "./assets/config";
+
+const client = new ApolloClient({
+  uri: `${config.apiHost}/graphql`,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
